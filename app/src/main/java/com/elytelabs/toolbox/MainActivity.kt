c7d66e799617f24065d7d6ef59e639aa699dd50e
ItzFaisal <thisizfaisal@outlook.com>
2025-01-14 11:31:59 +0530

    initial commit

--- app/src/main/java/com/elytelabs/toolbox/MainActivity.kt
+++ app/src/main/java/com/elytelabs/toolbox/MainActivity.kt
@@ -6,10 +6,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.elytelabs.toolbox.UtilKit.copyText
 import com.elytelabs.toolbox.UtilKit.getAppVersionCode
 import com.elytelabs.toolbox.UtilKit.getAppVersionName
 import com.elytelabs.toolbox.UtilKit.sendFeedbackEmail
+import com.elytelabs.toolbox.UtilKit.shareText
 import com.elytelabs.toolbox.UtilKit.showLongToast
+import com.elytelabs.toolbox.UtilKit.showOrHide
 import com.elytelabs.toolbox.UtilKit.showToast
 
 class MainActivity : AppCompatActivity() {
@@ -24,8 +27,13 @@ class MainActivity : AppCompatActivity() {
         }
         val email = findViewById<Button>(R.id.email)
         email.setOnClickListener {
-            sendFeedbackEmail(this,
-                arrayOf("example@abc.com"), getString(R.string.app_name))
+            val  emailId = "example@abc.com"
+            if (emailId.isEmailValid()) {
+                sendFeedbackEmail(this,
+                    arrayOf(emailId), getString(R.string.app_name))
+            } else {
+                showToast("Invalid Email")
+            }
         }
 
         val getAppVersionName = findViewById<Button>(R.id.getAppVersionName)
@@ -36,6 +44,26 @@ class MainActivity : AppCompatActivity() {
         val getAppVersionCode = findViewById<Button>(R.id.getAppVersionCode)
         getAppVersionCode.setOnClickListener {
             showToast(getAppVersionCode(this).toString())
+        }
+
+        val shareText = findViewById<Button>(R.id.shareText)
+        shareText.setOnClickListener {
+            shareText(this, " Text to share" )
+        }
+
+        val copyText = findViewById<Button>(R.id.copyText)
+        copyText.setOnClickListener {
+            copyText(this," Text to copy" )
+        }
+
+        val openWebPage = findViewById<Button>(R.id.openWebPage)
+        openWebPage.setOnClickListener {
+            UtilKit.openWebPage(this, "https://elytelabs.blogspot.com/")
+        }
+
+        val hideBtn = findViewById<Button>(R.id.hideBtn)
+        hideBtn.setOnClickListener {
+            hideBtn.showOrHide(false)
         }
 
     }
